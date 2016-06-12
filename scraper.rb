3e38a7f22bf3782eddeb9e68a6f58323fb4ed9fc
Tony Bowden <tony@tmtm.com>
2016-06-12 11:25:25 +0100

    Switch to Category-based

--- scraper.rb
+++ scraper.rb
@@ -1,47 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
-require 'wikidata/fetcher'
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-require 'rest-client'
 require 'wikidata/fetcher'
 
-def noko_for(url)
-  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read) 
-end
-
-def wikinames8(url)
-  noko = noko_for(url)
-  colname = '姓名'
-  noko.xpath('//table[.//tr[1]//td[contains(.,"%s")]]' % colname).map do |table|
-    title = table.xpath('preceding-sibling::h2').last.text
-    wantcol = table.xpath('.//tr[1]//td').find_index { |td| td.text == colname }
-    names = table.xpath('.//tr/td[%d]//a[not(@class="new")]/@title' % (wantcol+1)).map(&:text)
-    puts "#{title}: #{names.count}"
-    names
-  end.flatten(1)
-end
-
-def wikinames9(url)
-  noko = noko_for(url)
-  colname = '姓名'
-  noko.xpath('//table[.//tr[1]//th[contains(.,"%s")]]' % colname).map do |table|
-    title = table.xpath('preceding-sibling::h2').last.text
-    wantcol = table.xpath('.//tr[1]//th').find_index { |td| td.text == colname }
-    names = table.xpath('.//tr/td[%d]//a[not(@class="new")]/@title' % (wantcol+1)).map(&:text)
-    puts "#{title}: #{names.count}"
-    names
-  end.flatten(1)
-end
-
-names_8 = wikinames8('https://zh.wikipedia.org/wiki/第8屆中華民國立法委員名單')
-abort "No names for term 8" if names_8.count.zero?
-
-names_9 = wikinames9('https://zh.wikipedia.org/wiki/第9屆中華民國立法委員名單')
-abort "No names for term 9" if names_9.count.zero?
+names_8 = WikiData::Category.new( 'Category:第8屆中華民國立法委員', 'zh').member_titles
+names_9 = WikiData::Category.new( 'Category:第9屆中華民國立法委員', 'zh').member_titles
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { zh: names_8 | names_9 }, output: false)
 
