2dfcc75982e4b5b518c6e1166c617b4d8066d107
Tony Bowden <tony@tmtm.com>
2018-08-18 06:53:24 +0100

    Also fetch via P39s

--- scraper.rb
+++ scraper.rb
@@ -9,4 +9,7 @@ names_9 = WikiData::Category.new( 'Category:第9屆中華民國立法委員', 'z
 names_8_en = WikiData::Category.new( 'Category:Members of the 8th Legislative Yuan', 'en').member_titles
 names_9_en = WikiData::Category.new( 'Category:Members of the 9th Legislative Yuan', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { zh: names_8 | names_9, en: names_8_en | names_9_en }, output: false)
+sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39/ps:P39 wd:Q6310593 }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { zh: names_8 | names_9, en: names_8_en | names_9_en })
